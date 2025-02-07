9a10ce6827581b5b9e1132663f5193526af587f2
KarlDarlin <nzatsikondji404@gmail.com>
2025-02-07 12:12:40 +0100

    ajout

--- TPJAVAF/src/karlnoellecamille/AppareilFactory.java
+++ TPJAVAF/src/karlnoellecamille/AppareilFactory.java
@@ -1,5 +1,14 @@
 package karlnoellecamille;
 
-public class AppareilFactory {
-
+class AppareilFactory {
+    public static Appareil createAppareil(AppareilType type, String identifiant, String nom, double prixBase) {
+        switch (type) {
+            case ORDINATEUR:
+                return new Ordinateur(identifiant, nom, prixBase);
+            case IMPRIMANTE:
+                return new Imprimante(identifiant, nom, prixBase);
+            default:
+                throw new IllegalArgumentException("Type d'appareil inconnu : " + type);
+        }
+    }
 }
--- TPJAVAF/src/karlnoellecamille/Main.java
+++ TPJAVAF/src/karlnoellecamille/Main.java
@@ -2,33 +2,40 @@ package karlnoellecamille;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // test1
+        System.out.println("=== Test de l'Inventaire (Singleton) ===");
         Inventaire inventaire = Inventaire.getInstance();
 
-        
-        Appareil ordinateur = new Ordinateur("ORD123", "MacBook Pro", 1500.0);
-        Appareil imprimante = new Imprimante("IMP456", "HP LaserJet", 300.0);
+        // test2
+        System.out.println("\n=== Création des appareils via la Factory ===");
+        Appareil ordinateur1 = AppareilFactory.createAppareil(AppareilType.ORDINATEUR, "ORD123", "MacBook Pro", 1500.0);
+        Appareil imprimante1 = AppareilFactory.createAppareil(AppareilType.IMPRIMANTE, "IMP456", "HP LaserJet", 300.0);
+        Appareil ordinateur2 = AppareilFactory.createAppareil(AppareilType.ORDINATEUR, "ORD789", "Dell XPS", 1800.0);
 
-       
-        inventaire.ajouterAppareil(ordinateur);
-        inventaire.ajouterAppareil(imprimante);
+        //test 3
+        inventaire.ajouterAppareil(ordinateur1);
+        inventaire.ajouterAppareil(imprimante1);
+        inventaire.ajouterAppareil(ordinateur2);
 
     
         inventaire.afficherInventaire();
 
-        
+        // test 4
+        System.out.println("\n=== Suppression d'un appareil ===");
         inventaire.supprimerAppareil("IMP456");
-
-        
         inventaire.afficherInventaire();
 
-        
-        Appareil ordinateurSecurise = new OptionSecurite(ordinateur);
-        Appareil ordinateurOptimise = new OptionPerformance(ordinateurSecurise);
+        // test 5
+        System.out.println("\n=== Personnalisation d'un appareil avec le Decorator ===");
+
+      
+        Appareil ordinateurSecurise = new OptionSecurite(ordinateur1); 
+        Appareil ordinateurOptimise = new OptionPerformance(ordinateurSecurise); 
 
         
-        System.out.println("\nDétails après personnalisation :");
         System.out.println(ordinateurOptimise.getDescription());
         System.out.println("Prix final : " + ordinateurOptimise.getPrixFinal() + "€");
+
+       
     }
 }
